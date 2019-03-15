37eca2bf7a46e5c200ad9460a9ec1beb6e5cd612
Wayne Wilson <waynewilson10@gmail.com>
2019-03-15 00:47:23 -0600

    initial commit

---
+++ Barcode_test/Barcode_test/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Barcode_test.Models;
+
+namespace Barcode_test.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Barcode_test/Barcode_test/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Barcode_test.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
